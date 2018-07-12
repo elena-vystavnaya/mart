b56126c330e993ed3ef030d7928728f2d018c414
elena-vystavnaya <helenv2909@gmail.com>
2018-07-12 21:14:09 +0300

    new section

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$('.block-clothes.first').mouseover(function(){
+    $('.block-clothes-hover.first1').css('display', 'block');
+    //$('.link > h3').css('background', '#1abc9c');
+   // $('span').css('background', '#7fd9c7');
+
+});
+
