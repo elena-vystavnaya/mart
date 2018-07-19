c508c32fcf19d43efefeaf6fe6b1ce99ed855cb6
elena-vystavnaya <helenv2909@gmail.com>
2018-07-20 01:28:24 +0300

    add hover, animation, fixed menu, slider

--- js/index.js
+++ js/index.js
@@ -1,10 +1,51 @@
-new WOW().init();
-$('.block-clothes').mouseover(function(){
-    $(this).find('.block-clothes-hover').show();
-});
+$(document).ready(function(){
+    new WOW().init();
 
-$('.block-clothes').mouseleave(function(){
-    $(this).find('.block-clothes-hover').hide();
+    var navHeight = 49;
+    var nav = $('.header-menu');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > navHeight ) {
+            nav.addClass("fixed");
+        } else {
+            nav.removeClass("fixed");
+        }
+    });
+
+    $('.product').mouseover(function(){
+        $(this).find('.block-hover').show();
+        $(this).find('h3').css("background", "#46bfa9");
+        $(this).find('span').css("background", "#a3f3e2");
+    });
+
+    $('.product').mouseleave(function(){
+        $(this).find('.block-hover').hide();
+        $(this).find('h3').css("background", "#323232");
+        $(this).find('span').css("background", "#8c8c8c");
+    });
+
+    $("#menu").click(function(){
+        $("#list").slideToggle(1000);
+    });
+
+    var slider = $('.slider-carousel');
+    slider.owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        smartSpeed:1000,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true
+    });
+
+    var review = $('.review-carousel');
+    review.owlCarousel({
+        items:1,
+        dotsContainer: '#carousel-custom-dots'
+    });  
+
+   /* $('.owl-dot').click(function () {
+        owl.trigger('to.owl.carousel', [$(this).index(), 300]);
+      });*/
 });
 
 
